15ac2d623e65e7a68b2c2558bf5d0997328a7928
thanator <www.godx@mail.ru>
2017-05-16 20:41:07 +0300

    v0.62 ass

--- app/src/main/java/com/tan_ds/testapp/MainActivity.java
+++ app/src/main/java/com/tan_ds/testapp/MainActivity.java
@@ -11,6 +11,10 @@ import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends Activity {
 
 
@@ -20,23 +24,41 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         AssyncTaskimpl ass = new AssyncTaskimpl();
-        ass.execute();
+        ass.execute("AAAAA", "ccccc");
 
 
     }
 
-    private class AssyncTaskimpl extends AsyncTask <Void, Void, Void>{
+    private class AssyncTaskimpl extends AsyncTask <String, Integer, Double>{
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Double doInBackground(String... params) {
+            List<String> listParams = Arrays.asList(params);
 
             Log.v("Thread work", "back: " + Thread.currentThread().getId());
-            return null;
+            Log.v("Thread work", "back: " + listParams);
+
+            for (Integer i = 0; i < 10 ; i++){
+                publishProgress(i, i+1);
+            }
+
+            Random random = new Random();
+
+
+            return random.nextDouble();
         }
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.v("Thread work", "update: " + Arrays.toString(values));
+
+        }
+
+        @Override
+        protected void onPostExecute(Double result) {
+            super.onPostExecute(result);
+            Log.v("Thread work", "Postexecute: " + result);
 
             Log.v("Thread work", "Postexecute: " + Thread.currentThread().getId());
         }
